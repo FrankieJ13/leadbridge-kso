d5de73a7fec745c1aca3985a2d31aece1ebb6ef8
FrankieJ13 <frankie13.jung@gmail.com>
2026-08-16 16:22:19 +0500

    Rebuild browser OCR packages

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts('./src/security.js');
 
-const CACHE_NAME = 'leadbridge-kso-pwa-v8.2.10.0848-20';
+const CACHE_NAME = 'leadbridge-kso-pwa-v8.2.10.0848-21';
 const APP_SHELL = [
   './index.html',
   './app.css',
